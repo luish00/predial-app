238f803b2e689dd90e6cde7781085b7005a86769
OFCL8 <oliver.carrillo@tectijuana.edu.mx>
2022-11-23 07:57:17 -0800

    Added camera feature

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,19 +1,29 @@
 import accountIcon from './account.png';
+import backWhiteIcon from './back-white.png';
+import checkIcon from './check.png';
+import circleWhiteIcon from './circle-white.png';
+import closeWhiteIcon from './close-white.png';
 import editIcon from './edit.png';
 import emailIcon from './email.png';
 import passwordIcon from './password.png';
+import repeatWhiteIcon from './repeat-white.png';
 import saveIcon from './save.png';
 import searchIcon from './search.png';
-import checkIcon from './check.png';
+import unCheckedRadioWhiteIcon from './radio-button-unchecked-white.png';
 import unCheckIcon from './uncheck.png';
 
 export {
   accountIcon,
+  backWhiteIcon,
   checkIcon,
+  circleWhiteIcon,
+  closeWhiteIcon,
   editIcon,
   emailIcon,
   passwordIcon,
+  repeatWhiteIcon,
   saveIcon,
   searchIcon,
+  unCheckedRadioWhiteIcon,
   unCheckIcon,
 };
